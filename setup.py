3f4d4630b0f66915091a9eaa0ae45f164e65ab24
Paul Singman <paul.singman@equinox.com>
2019-08-26 17:07:48 -0400

    rm fancy req stuff

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from distutils.core import setup
-import os
-
-def read_requirements():
-    """Parse requirements from requirements.txt."""
-    reqs_path = os.path.join('.', 'requirements.txt')
-    with open(reqs_path, 'r') as f:
-        requirements = [line.rstrip() for line in f]
-    return requirements
 
 setup(
   name = 'cocore',
@@ -19,5 +11,10 @@ setup(
   url = 'https://github.com/equinoxfitness/datacoco.core',
   download_url = f'https://github.com/equinoxfitness/datacoco.core/archive/0.4.tar.gz',
   keywords = ['helper', 'config', 'logging', 'common'],   # Keywords that define your package best
-  install_requires=read_requirements()
+  install_requires=[
+      'requests==2.20.0'
+      'grequests==0.3.0',
+      'future==0.16.0',
+      'boto3==1.9.203'
+    ]
 )
