81714b9c4074adae2f6a6118196f6f02e0126967
Paul Singman <paul.singman@equinox.com>
2019-08-27 09:21:05 -0400

    rm class

--- cocore/config.py
+++ cocore/config.py
@@ -6,25 +6,20 @@ from future import standard_library
 standard_library.install_aliases()
 from future.builtins import str
 
-class Config:
+def Config(conf_path='etl.cfg'):
+    """Reads config file and returns a dictionary of config objects.
 
-    def __init__(self, conf_path):
-        self.conf_path = conf_path
-
-    def conf(self):
-        """Reads config file and returns a dictionary of config objects.
-
-        :param conf_path: Absolute path to configuration file.
-        :return: Dictionary with configuration arguments and values
-        """
-        config = ConfigParser()
-        config.read(self.conf_path)
-        dictionary = {}
-        for section in config.sections():
-            dictionary[section] = {}
-            for option in config.options(section):
-                val = config.get(section, option)
-                if str(option).lower() in ('pwd', 'password'):
-                    val = str(base64.b64decode(val), 'utf-8')
-                dictionary[section][option] = val
-        return dictionary
+    :param conf_path: Absolute path to configuration file.
+    :return: Dictionary with configuration arguments and values
+    """
+    config = ConfigParser()
+    config.read(conf_path)
+    dictionary = {}
+    for section in config.sections():
+        dictionary[section] = {}
+        for option in config.options(section):
+            val = config.get(section, option)
+            if str(option).lower() in ('pwd', 'password'):
+                val = str(base64.b64decode(val), 'utf-8')
+            dictionary[section][option] = val
+    return dictionary
